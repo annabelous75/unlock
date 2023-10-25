554de050c98e15e3e88c10d7c0f8db4631c075ff
annabelous75 <57856008+annabelous75@users.noreply.github.com>
2023-10-25 19:33:34 +0300

    /

---
+++ js/script.js
@@ -0,0 +1,50 @@
+$(document).ready(function(){
+	$('.slider').slick({
+		arrows:true,
+		dots:true,
+		slidesToShow:3,
+		autoplay:true,
+		speed:1000,
+		focusOnSelect:true,
+		autoplaySpeed:3000,
+		responsive:[
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow:2
+				}
+			},
+			{
+				breakpoint: 550,
+				settings: {
+					slidesToShow:1
+				}
+			}
+		]
+	});
+});
+$(document).ready(function(){
+	$('.tours').slick({
+		arrows:true,
+		dots:true,
+		slidesToShow:2,
+		autoplay:true,
+		speed:1000,
+		focusOnSelect:true,
+		autoplaySpeed:3000,
+		responsive:[
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow:1
+				}
+			},
+			{
+				breakpoint: 550,
+				settings: {
+					slidesToShow:1
+				}
+			}
+		]
+	});
+});
